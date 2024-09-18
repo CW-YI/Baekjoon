08d1039be842e7860a073cf4b766ff610d09fe99
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-09-18 16:53:41 +0900

    [Silver II] Title: 쇠막대기, Time: 4 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(void) {
+	string input;
+	stack<char> s;
+	int num = 0;
+
+	cin >> input;
+	for (int i = 0; i < input.length(); i++) {
+		if (input[i] == '(' && input[i+1] == ')') {
+			num += s.size();
+			i++;
+		}
+		else if (input[i] == '(') s.push(input[i]);
+		else {
+			s.pop();
+			num += 1;
+		}
+	}
+	
+	cout << num;
+
+	return 0;
+}
