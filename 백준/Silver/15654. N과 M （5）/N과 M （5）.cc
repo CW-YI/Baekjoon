070d53615ffc11649b979636e074241478055458
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-10-07 13:23:02 +0900

    [Silver III] Title: N과 M (5), Time: 28 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int M;
+
+void printNum(int start, int end, vector<int> v, vector<int> number, int index) {
+	if (start > end) return;
+
+	for (int i = start; i < end; i++) {
+		v[index] = number[i];
+		swap(number[i], number[index]);
+
+		if (index + 1 < M) printNum(start + 1, end, v, number, index + 1);
+
+		if (index == M - 1) {
+			for (int j = 0; j < M; j++) cout << v[j] << " ";
+			cout << "\n";
+		}
+	}
+}
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N, temp;
+	cin >> N >> M;
+
+	vector<int> number;
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+		number.push_back(temp);
+	}
+
+	sort(number.begin(), number.end());
+	vector<int> v(M, 0);
+
+	printNum(0, N, v, number, 0);
+		
+	return 0;
+}
