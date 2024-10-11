7e7233ab3ffbca3ce4f4c11525e492e7dc1c04ab
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-10-11 17:38:14 +0900

    [Gold IV] Title: 알고리즘 수업 - 선택 정렬 4, Time: 596 ms, Memory: 27476 KB -BaekjoonHub

---
+++ 백준/Gold/23884. 알고리즘 수업 － 선택 정렬 4/알고리즘 수업 － 선택 정렬 4.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N, K;
+
+	cin >> N >> K;
+	map<int, int, greater<int>> hm;
+	vector<int> v(N, 0);
+
+	int temp;
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+		hm.insert(make_pair(temp, i));
+		v[i] = temp;
+	}
+
+	int index = N;
+	for (const auto& pair : hm) {
+		--index;
+		if (pair.second == index) continue;
+
+		hm[v[index]] = pair.second;
+		swap(v[pair.second], v[index]);
+		
+		K--;
+
+		if (K == 0) {
+			for (int i = 0; i < N; i++) cout << v[i] << " ";
+			return 0;
+		}
+	}
+
+	cout << -1;
+	return 0;
+}
