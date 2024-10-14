19bb12511100d46199e13dc906ff042dfb861e14
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-10-14 13:40:10 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 1, Time: 76 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/24051. 알고리즘 수업 － 삽입 정렬 1/알고리즘 수업 － 삽입 정렬 1.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, K;
+
+void insert_sort(vector<int>& A) {
+	for (int i = 1; i < N; i++) {
+		int loc = i - 1;
+		int newItem = A[i];
+
+		// 이 지점에서 A[i-1]까지 정렬 되어있는 상태
+		while (0 <= loc && newItem < A[loc]) {
+			A[loc + 1] = A[loc];
+			loc--;
+			K--;
+			if (K <= 0) {
+				cout << A[loc + 1];
+				exit(0);
+			}
+		}
+
+		if (loc + 1 != i) {
+			A[loc + 1] = newItem;
+			K--;
+			if (K <= 0) {
+				cout << A[loc + 1];
+				exit(0);
+			}
+		}
+	}
+}
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> N >> K;
+
+	vector<int> A(N);
+	for (int i = 0; i < N; i++) cin >> A[i];
+
+	insert_sort(A);
+	if (K > 0) cout << -1;
+	return 0;
+}
