203a98013e10058650aec41a7ec12628ad234a35
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-10-16 15:33:23 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int M;
+
+void printNum(int start, int end, vector<int> v, int index) {
+	if (start > end) return;
+
+	for (int i = start; i <= end; i++) {
+		v[index] = i;
+		if (index + 1 < M) printNum(i, end, v, index + 1);
+
+		if (index == M - 1) {
+			for (int j = 0; j <= index; j++) cout << v[j] << " ";
+			cout << "\n";
+		}
+	}
+}
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N >> M;
+
+	vector<int> v(M, 0);
+	printNum(1, N, v, 0);
+		
+	return 0;
+}
