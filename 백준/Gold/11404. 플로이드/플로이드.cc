f6ffa959680bb1a4dfbf24240a7a7f698fda71ca
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-11-04 13:03:00 +0900

    [Gold IV] Title: 플로이드, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -3,46 +3,33 @@
 #include <queue>
 using namespace std;
 
+const int INF = 1e9;
+
 int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<pair<int, int>>> v(n + 1);
+    vector<vector<int>> v(n + 1, vector<int>(n + 1, INF));
     int a, b, c;
+
+    for (int i = 1; i <= n; i++)
+        v[i][i] = 0;
 
     for (int i = 0; i < m; i++) {
         cin >> a >> b >> c;
-        v[a].push_back(make_pair(b, c));
+        v[a][b] = min(v[a][b], c);
     }
 
-    vector<vector<int>> result(n + 1, vector<int>(n + 1, -1));
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-
-    for (int i = 1; i <= n; i++) {
-        result[i][i] = 0;
-        q.push(make_pair(0, i));
-
-        while (!q.empty()) {
-            int node = q.top().second;
-            int weight = q.top().first;
-            int size = v[node].size();
-
-            q.pop();
-            for (int j = 0; j < size; j++) {
-                int nextNode = v[node][j].first;
-                int nextWeight = v[node][j].second;
-
-                if (result[i][nextNode] != -1 && result[i][nextNode] < weight + nextWeight) continue;
-                result[i][nextNode] = weight + nextWeight;
-                q.push(make_pair(weight + nextWeight, nextNode));
-            }
-        }
-    }
+    for (int k = 1; k <= n; k++)
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+                if (v[i][k] != INF && v[k][j] != INF)
+                    v[i][j] = min(v[i][j], v[i][k] + v[k][j]);
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            if (result[i][j] == -1) cout << "0 ";
-            else cout << result[i][j] << " ";
+            if (v[i][j] == INF) cout << "0 ";
+            else cout << v[i][j] << " ";
         }
         cout << "\n";
     }
