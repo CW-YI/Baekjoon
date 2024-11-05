82d24052714966ef3fc0ead219979e6ded667e99
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-11-05 13:41:26 +0900

    [Gold III] Title: 파티, Time: 112 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,52 +1,61 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <vector>
 using namespace std;
+
+const int INF = 1e9;
+vector<vector<pair<int, int>>> v;
+int N;
+
+vector<int> dijkstra(int num) {
+    vector<int> dist(N + 1, INF);
+    priority_queue < pair<int, int>, vector<pair<int, int>>, greater < pair<int, int>>> q;
+
+    q.push(make_pair(0, num));
+
+    while (!q.empty()) {
+        int node = q.top().second;
+        int w = q.top().first;
+
+        q.pop();
+
+        for (const auto& pair : v[node]) {
+            int nextNode = pair.first;
+            int nextW = pair.second;
+
+            if (dist[nextNode] > nextW + w) {
+                dist[nextNode] = nextW + w;
+                q.push(make_pair(nextW + w, nextNode));
+            }
+        }
+    }
+    return dist;
+}
 
 int main(void) {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    int N, M, X;
+    int M, X;
     cin >> N >> M >> X;
 
-    vector<vector<int>> v(N + 1, vector<int>(N + 1, -1));
+    v.resize(N + 1);
     int start, end, weight;
     for (int i = 0; i < M; i++) {
         cin >> start >> end >> weight;
-        v[start][end] = weight;
+        v[start].emplace_back(make_pair(end, weight));
     }
 
-    vector<vector<int>> visit(N + 1, vector<int>(N + 1, -1));
-    priority_queue < pair<int, int>, vector<pair<int, int>>, greater < pair<int, int>>> q;
+    vector<int> XToNode = dijkstra(X);
+    vector<int> NodeToX(N + 1);
+    for (int i = 1; i <= N; i++)
+        NodeToX[i] = dijkstra(i)[X];
 
-    for (int i = 1; i <= N; i++) {
-        q.push(make_pair(0, i));
+    NodeToX[X] = XToNode[X] = 0;
+    int maxNum = 0;
+    for (int i = 1; i <= N; i++)
+        maxNum = max(maxNum, NodeToX[i] + XToNode[i]);
 
-        while (!q.empty()) {
-            int node = q.top().second;
-            int w = q.top().first;
-
-            q.pop();
-
-            for (int j = 1; j <= N; j++) {
-                int nextW = v[node][j];
-                if (v[node][j] == -1 || (visit[i][j] != -1 && visit[i][j] < w + nextW))
-                    continue;
-                visit[i][j] = w + nextW;
-                q.push(make_pair(w + nextW, j));
-            }
-        }
-    }
-
-    int max = 0;
-    for (int i = 1; i <= N; i++) {
-        if (i == X) continue;
-        int num = visit[i][X] + visit[X][i];
-        if (max < num) max = num;
-    }
-
-    cout << max;
+    cout << maxNum;
     return 0;
 }
