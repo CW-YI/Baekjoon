4a24d72aca62164f30d3c02cc662f8cc9451ff35
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-11-05 13:59:19 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2424 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -4,10 +4,9 @@
 using namespace std;
 
 const int INF = 1e9;
-vector<vector<pair<int, int>>> v;
 int N;
 
-vector<int> dijkstra(int num) {
+vector<int> dijkstra(int num, vector<vector<pair<int, int>>> v) {
     vector<int> dist(N + 1, INF);
     priority_queue < pair<int, int>, vector<pair<int, int>>, greater < pair<int, int>>> q;
 
@@ -33,23 +32,24 @@ vector<int> dijkstra(int num) {
 }
 
 int main(void) {
-    cin.tie(NULL);
+    cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
     int M, X;
     cin >> N >> M >> X;
 
-    v.resize(N + 1);
+    vector<vector<pair<int, int>>> v, rv;
+    v.resize(N + 1); rv.resize(N + 1);
+
     int start, end, weight;
     for (int i = 0; i < M; i++) {
         cin >> start >> end >> weight;
         v[start].emplace_back(make_pair(end, weight));
+        rv[end].emplace_back(make_pair(start, weight));
     }
 
-    vector<int> XToNode = dijkstra(X);
-    vector<int> NodeToX(N + 1);
-    for (int i = 1; i <= N; i++)
-        NodeToX[i] = dijkstra(i)[X];
+    vector<int> XToNode = dijkstra(X, v);
+    vector<int> NodeToX = dijkstra(X, rv);
 
     NodeToX[X] = XToNode[X] = 0;
     int maxNum = 0;
