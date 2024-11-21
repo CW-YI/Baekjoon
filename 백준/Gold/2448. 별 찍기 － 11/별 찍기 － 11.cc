60ee24fef12a178075dea3d06e71f84a175aa2ae
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-11-21 11:46:35 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 384 ms, Memory: 20808 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.cc
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.cc
@@ -3,12 +3,12 @@
 #include <cmath>
 
 using namespace std;
-int** star;
+vector<vector<char>> star;
 
 void basic_star(int i, int j) { // 기준 위치는 제일 왼쪽 끝 아래
-    for (int k = 0; k < 5; k++) star[i][j + k] = 1;
-    star[i - 1][j + 1] = 1; star[i - 1][j + 3] = 1;
-    star[i - 2][j + 2] = 1;
+    for (int k = 0; k < 5; k++) star[i][j + k] = '*';
+    star[i - 1][j + 1] = '*'; star[i - 1][j + 3] = '*';
+    star[i - 2][j + 2] = '*';
 }
 
 void rBuildStar(int i, int j, int size) { // size는 0 - k
@@ -25,24 +25,17 @@ void rBuildStar(int i, int j, int size) { // size는 0 - k
 int main() {
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    
-    int N;
 
+    int N;
     cin >> N;
-    star = new int* [N];
-    for (int i = 0; i < N; i++) star[i] = new int[(N / 3) * 6];
+    star.resize(N, vector<char>((N / 3) * 6, ' '));
 
     rBuildStar(N - 1, 0, log2(N / 3));
 
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < (N / 3) * 6; j++) {
-            if (star[i][j] == 1) cout << "*";
-            else cout << " ";
-        }
+        for (int j = 0; j < (N / 3) * 6; j++)
+            cout << star[i][j];
         cout << "\n";
     }
-
-    for (int i = 0; i < N; i++) delete[] star[i];
-    delete[] star;
     return 0;
 }
