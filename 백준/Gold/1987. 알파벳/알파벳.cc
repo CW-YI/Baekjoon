aed37cc9a04099be0efca6125606712d8ad084c8
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-11-25 22:58:25 +0900

    [Gold IV] Title: 알파벳, Time: 724 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,51 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <bitset>
 
 using namespace std;
 int N, M;
 vector<vector<char>> v;
+vector<bool> visit(26, false);
 int dir[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
 
-int bfs() {
-    queue<pair<pair<int, int>, int >> q;
-    q.push({ {0, 0}, 1 << (v[0][0] - 'A') });
-    unsigned int depth = 1;
-    int dep = 1;
+int dp(int x, int y, int depth) {
+    int num = depth;
+    for (int d = 0; d < 4; d++) {
+        int dx = x + dir[d][0], dy = y + dir[d][1];
+        if (dx < 0 || dx >= N || dy < 0 || dy >= M) continue;
 
-    while (!q.empty()) {
-        int x = q.front().first.first, y = q.front().first.second;
-        int visit = q.front().second;
-        q.pop();
-
-        for (int i = 0; i < 4; i++) {
-            int dx = x + dir[i][0], dy = y + dir[i][1];
-            if (dx < 0 || dx >= N || dy < 0 || dy >= M) continue;
-            
-            int index = v[dx][dy] - 'A';
-            if ((visit & (1 << index)) == 0) {
-                q.push({ {dx, dy}, visit | (1 << index) });
-
-                #if defined(_MSC_VER)
-                    depth = max(depth, __popcnt(visit | (1 << index)));
-                #else
-                    dep = max(dep, __builtin_popcount(visit | (1 << index)));
-                #endif
-            }
+        if (!visit[v[dx][dy] - 'A']) {
+            visit[v[dx][dy] - 'A'] = true;
+            int temp = dp(dx, dy, depth + 1);
+            num = max(num, temp);
+            visit[v[dx][dy] - 'A'] = false;
         }
     }
-    #if defined(_MSC_VER)
-        return depth;
-    #else
-        return dep;
-    #endif
+    return num;
 }
 
 int main() {
     cin.tie(NULL); cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    
+
     cin >> N >> M;
     v.resize(N, vector<char>(M));
 
@@ -56,6 +37,7 @@ int main() {
             v[i][j] = input[j];
     }
 
-    cout << bfs();
+    visit[v[0][0] - 'A'] = true;
+    cout << dp(0, 0, 1);
     return 0;
 }
