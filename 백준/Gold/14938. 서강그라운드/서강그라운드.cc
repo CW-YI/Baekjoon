76da48c96a1b7bcb7e4a0895ee28cd7d89417063
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-12-01 14:41:25 +0900

    [Gold IV] Title: 서강그라운드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+int const INF = 1e9;
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N, M, R;
+    cin >> N >> M >> R;
+
+    vector<pair<int, vector<pair<int, int>>>> v(N + 1);
+
+    for (int i = 1; i <= N; i++)
+        cin >> v[i].first;
+
+    for (int i = 0, s, e, w; i < R; i++) {
+        cin >> s >> e >> w;
+        v[s].second.push_back({ e, w });
+        v[e].second.push_back({ s, w });
+    }
+
+    int maxNum = 0;
+    for (int i = 1; i <= N; i++) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+        vector<int> visit(N + 1, INF);
+
+        q.push({ i, 0 });
+        visit[i] = v[i].first;
+
+        while (!q.empty()) {
+            int curNode = q.top().first;
+            int curDistance = q.top().second;
+            q.pop();
+
+            if (visit[curNode] < curDistance) continue;
+
+            for (const auto& nextPair : v[curNode].second) {
+                int nextNode = nextPair.first;
+                int nextWeight = nextPair.second;
+
+                if (curDistance + nextWeight > M) continue;
+                if (visit[nextNode] < curDistance + nextWeight) continue;
+
+                q.push({ nextNode, curDistance + nextWeight });
+                visit[nextNode] = curDistance + nextWeight;
+            }
+        }
+
+        int totalNum = 0;
+        for (int j = 1; j <= N; j++)
+            if (visit[j] != INF) totalNum += v[j].first;
+
+        maxNum = max(maxNum, totalNum);
+    }
+
+    cout << maxNum;
+    return 0;
+}
