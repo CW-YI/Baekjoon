282c639809705d13a50fec08e17fe74e6ae886ac
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-12-01 16:41:59 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -2,22 +2,24 @@
 #include <vector>
 
 using namespace std;
+
+vector<vector<vector<int>>> dp;
 vector<vector<int>> v;
 int N;
 
-int findSolution(pair<int, int> startPos, pair<int, int> endPos, int way) { // 가로 0 세로 1 대각선 2
-    if (endPos == make_pair(N - 1, N - 1)) return 1;
+int findSolution(int x, int y, int way) {
+    if (x == N - 1 && y == N - 1) return 1; 
+    if (dp[x][y][way] != -1) return dp[x][y][way];
 
     int n1 = 0, n2 = 0, n3 = 0;
-    if (way != 1 && endPos.second + 1 < N && v[endPos.first][endPos.second + 1] == 0)
-        n1 = findSolution(endPos, { endPos.first, endPos.second + 1 }, 0);
-    if (way != 0 && endPos.first + 1 < N && v[endPos.first + 1][endPos.second] == 0)
-        n2 = findSolution(endPos, { endPos.first + 1, endPos.second}, 1);
-    if (endPos.first + 1 < N && endPos.second + 1 < N)
-        if (v[endPos.first + 1][endPos.second] == 0 && v[endPos.first][endPos.second + 1] == 0 && v[endPos.first + 1][endPos.second + 1] == 0)
-            n3 = findSolution(endPos, { endPos.first + 1, endPos.second + 1 }, 2);
+    if (way != 1 && y + 1 < N && v[x][y + 1] == 0)
+        n1 = findSolution(x, y + 1, 0);
+    if (way != 0 && x + 1 < N && v[x + 1][y] == 0)
+        n2 = findSolution(x + 1, y, 1);
+    if (x + 1 < N && y + 1 < N && v[x + 1][y] == 0 && v[x][y + 1] == 0 && v[x + 1][y + 1] == 0)
+        n3 = findSolution(x + 1, y + 1, 2);
 
-    return n1 + n2 + n3;
+    return dp[x][y][way] = n1 + n2 + n3;
 }
 
 int main() {
@@ -27,10 +29,12 @@ int main() {
     cin >> N;
 
     v.resize(N, vector<int>(N));
+    dp.resize(N, vector<vector<int>>(N, vector<int>(3, -1)));
+
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             cin >> v[i][j];
 
-    cout << findSolution({ 0, 0 }, { 0, 1 }, 0);
+    cout << findSolution(0, 1, 0);
     return 0;
 }
