5dd26a1cc85ed1f9874c5b2d4e4e165a303598d3
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-12-01 15:43:26 +0900

    [Gold IV] Title: 연구소, Time: 44 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -4,9 +4,16 @@
 
 using namespace std;
 int dir[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+vector<pair<int, int>> emptySpace, virusList;
+vector<vector<int>> v;
 int N, M;
 
-int findNum(vector<vector<int>> v, queue<pair<int, int>> q) {
+int findNum() {
+    queue<pair<int, int>> q;
+    vector<vector<int>> temp = v;
+    for (const auto& virusPos : virusList)
+        q.push(virusPos);
+
     while (!q.empty()) {
         auto [curX, curY] = q.front();
         q.pop();
@@ -15,9 +22,9 @@ int findNum(vector<vector<int>> v, queue<pair<int, int>> q) {
             int nextX = curX + dir[i][0], nextY = curY + dir[i][1];
 
             if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) continue;
-            if (v[nextX][nextY] != 0) continue;
+            if (temp[nextX][nextY] != 0) continue;
 
-            v[nextX][nextY] = 2;
+            temp[nextX][nextY] = 2;
             q.push({ nextX, nextY });
         }
     }
@@ -25,7 +32,7 @@ int findNum(vector<vector<int>> v, queue<pair<int, int>> q) {
     int totalZero = 0;
     for (int i = 0; i < N; i++)
         for (int j = 0; j < M; j++)
-            if (v[i][j] == 0) totalZero++;
+            if (temp[i][j] == 0) totalZero++;
 
     return totalZero;
 }
@@ -35,39 +42,29 @@ int main() {
     ios_base::sync_with_stdio(false);
     
     cin >> N >> M;
-
-    vector<vector<int>> v(N, vector<int>(M));
-    queue<pair<int, int>> q;
+    v.resize(N, vector<int>(M));
+    
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             cin >> v[i][j];
-            if (v[i][j] == 2) q.push({ i, j });
+            if (v[i][j] == 0) emptySpace.push_back({ i, j });
+            if (v[i][j] == 2) virusList.push_back({ i, j });
         }
     }
 
     int maxNum = 0;
-    for (int x1 = 0; x1 < N; x1++) {
-        for (int y1 = 0; y1 < M; y1++) {
-            if (v[x1][y1] != 0) continue;
+    int emptySize = emptySpace.size();
+    for (int i = 0; i < emptySize; i++) {
+        for (int j = i + 1; j < emptySize; j++) {
+            for (int k = j + 1; k < emptySize; k++) {
+                auto [x1, y1] = emptySpace[i];
+                auto [x2, y2] = emptySpace[j];
+                auto [x3, y3] = emptySpace[k];
 
-            for (int x2 = 0; x2 < N; x2++) {
-                for (int y2 = 0; y2 < M; y2++) {
-                    if (v[x2][y2] != 0) continue;
-                    if (x1 == x2 && y1 == y2) continue;
-
-                    for (int x3 = 0; x3 < N; x3++) {
-                        for (int y3 = 0; y3 < M; y3++) {
-                            if (v[x3][y3] != 0) continue;
-                            if (x3 == x2 && y3 == y2) continue;
-                            if (x3 == x1 && y3 == y1) continue;
-
-                            v[x1][y1] = v[x2][y2] = v[x3][y3] = 1;
-                            maxNum = max(maxNum, findNum(v, q));
-                            v[x1][y1] = v[x2][y2] = v[x3][y3] = 0;
-                        }
-                    }
-                }
+                v[x1][y1] = v[x2][y2] = v[x3][y3] = 1;
+                maxNum = max(maxNum, findNum());
+                v[x1][y1] = v[x2][y2] = v[x3][y3] = 0;
             }
         }
     }
