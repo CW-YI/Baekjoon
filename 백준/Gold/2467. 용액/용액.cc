d6001a077648f36e9613212c8255be7249f016dd
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-12-04 13:53:51 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+const long long int INF = 3e9;
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+    vector<int> v(N, 0);
+
+    for (int i = 0; i < N; i++)
+        cin >> v[i];
+
+    // - ~ +
+    long long int minNum = INF;
+    int i = 0, j = N - 1;
+    int n1 = v[i], n2 = v[j];
+    while (i < j) {
+        int temp = v[i] + v[j];
+        if (abs(temp) < abs(minNum)) { minNum = temp; n1 = v[i], n2 = v[j]; }
+
+        if (temp < 0) i++;
+        else if (temp > 0) j--;
+        else break;
+    }
+
+    cout << n1 << " " << n2;
+    return 0;
+}
