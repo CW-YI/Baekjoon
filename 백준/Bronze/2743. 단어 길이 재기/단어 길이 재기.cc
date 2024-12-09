228b238e283e0f9f352ad60f531b8a08686f90c0
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-12-09 12:32:16 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string input;
+    cin >> input;
+    cout << input.size();
+    return 0;
+}
