489eb5b3106eaa04fab6e09265a899789240d8ab
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-12-10 11:57:30 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+    for (int i = 0, n1, n2; i < N; i++) {
+        cin >> n1 >> n2;
+        cout << n1 + n2 << "\n";
+    }
+    return 0;
+}
