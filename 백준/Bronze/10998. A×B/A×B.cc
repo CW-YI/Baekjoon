aadd5d856956885bf67c71db33a0008034a47593
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-12-13 23:28:14 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int N, M;
+    cin >> N >> M;
+    cout << N * M;
+    return 0;
+}
