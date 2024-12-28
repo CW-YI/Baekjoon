44c6146006ee0ce60d09429204438ecafd15b040
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-12-28 22:45:23 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N, M;
+    cin >> N >> M;
+    cout << N + M << "\n" << N - M << "\n" << N * M << "\n" << N / M << "\n" << N % M;
+
+    return 0;
+}
