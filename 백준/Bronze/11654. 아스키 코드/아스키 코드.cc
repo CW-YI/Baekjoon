af91045f8151b4f3ee6ad8f7d030508ec7c45cd6
CWYI <80385978+CW-YI@users.noreply.github.com>
2024-12-30 14:43:25 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    char ch;
+    cin >> ch;
+    cout << (int)(ch);
+    return 0;
+}
