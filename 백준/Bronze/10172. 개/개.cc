fdde15da293d82d4fbe1b11c95d1a9aae31a48c4
CWYI <80385978+CW-YI@users.noreply.github.com>
2025-01-05 15:17:26 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cout << "|\\_/|" << "\n";
+    cout << "|q p|   /}" << "\n";
+    cout << "( 0 )\"\"\"\\" << "\n";
+    cout << "|\"^\"`    |" << "\n";
+    cout << "||_/=\\\\__|" << "\n";
+    return 0;
+}
