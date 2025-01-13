8cd4fa1a87d61e4f48c3a0ddeb4bb41951824cc3
CWYI <80385978+CW-YI@users.noreply.github.com>
2025-01-13 14:21:41 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int A, B, C;
+    cin >> A >> B >> C;
+    cout << (A + B) % C << "\n" << ((A % C) + (B % C)) % C << "\n" << (A * B) % C << "\n" << ((A % C) * (B % C)) % C;
+
+    return 0;
+}
